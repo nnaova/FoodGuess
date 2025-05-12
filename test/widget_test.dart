c604a1a6ceb86be973fcf9c99b0a958afa794583
galexand <71892736+nnaova@users.noreply.github.com>
2025-05-12 16:26:57 +0200

    debug job

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:collecte/main.dart';
+import 'package:food_guess/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
